c732f478b6b556a9c20f4709eed06685ed9c6a6e
Eric Grossman <eric.josh.grossman@gmail.com>
2019-11-18 16:29:09 -0600

    scrollview keyboard

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,7 +1,17 @@
 import React, { useState, useContext } from 'react'
-import { Form, Item, Button, Label, Input, Text, Content } from 'native-base'
-import { KeyboardAvoidingView } from 'react-native'
+import {
+  Form,
+  Item,
+  Button,
+  Label,
+  Input,
+  Text,
+  Content,
+  Container,
+} from 'native-base'
+import { KeyboardAvoidingView, ScrollView } from 'react-native'
 import UserContext from '../context/UserContext'
+
 const CommentForm = ({ curWord }) => {
   const { curUser, curFriend, addComment } = useContext(UserContext)
   const [newComment, setNewComment] = useState('')
@@ -10,31 +20,33 @@ const CommentForm = ({ curWord }) => {
     setNewComment('')
   }
   return (
-    // <KeyboardAvoidingView behavior='padding' enabled>
-    <Content>
-      <Form>
-        <Text style={{ color: 'white' }}>
-          {curFriend.name} is feeling {curWord.value} because...
-        </Text>
-        <Item fixedLabel>
-          <Input
-            style={{ color: 'white' }}
-            placeholder='comment here'
-            autoCorrect={false}
-            value={newComment}
-            onChangeText={val => setNewComment(val)}
-          />
-          <Button
-            onPress={() => {
-              submitComment()
-            }}
-          >
-            <Text style={{ color: 'white' }}>Post</Text>
-          </Button>
-        </Item>
-      </Form>
-    </Content>
-    // </KeyboardAvoidingView>
+    <>
+      <KeyboardAvoidingView behavior='padding' enabled>
+        <ScrollView>
+          <Form>
+            <Text style={{ color: 'white' }}>
+              {curFriend.name} is feeling {curWord.value} because...
+            </Text>
+            <Item fixedLabel>
+              <Input
+                style={{ color: 'white' }}
+                placeholder='comment here'
+                autoCorrect={false}
+                value={newComment}
+                onChangeText={val => setNewComment(val)}
+              />
+              <Button
+                onPress={() => {
+                  submitComment()
+                }}
+              >
+                <Text style={{ color: 'white' }}>Post</Text>
+              </Button>
+            </Item>
+          </Form>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
   )
 }
 
